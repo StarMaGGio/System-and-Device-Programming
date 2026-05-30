ac0d47931a398ec81b82ceba8ecf66f85b781b16
Mattia Maggioni <mattiaethan6@gmail.com>
2026-05-30 09:34:19 +0200

    u07 exercise memory coaleshing

--- Exercices/u07-cudaParallelProgramming/s06-memoryCoalescing/e01/access_complex_data_struct.cu
+++ Exercices/u07-cudaParallelProgramming/s06-memoryCoalescing/e01/access_complex_data_struct.cu
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+#define N 1024
+
 typedef struct node_s {
     char a;
     int b;
@@ -30,7 +32,7 @@ typedef struct node_s_v {
  * there is one only object with all fields in its proper array.
  * [a, a, a, ..., b, b, b, ..., c, c, c, ...] so also in memory they will be stored in a contiguous way.
  */
-__global__ void mykernel (char *va, int *vb, float *vc) {
+__global__ void mykernel_v (char *va, int *vb, float *vc) {
     unsigned idx = blockIdx.x * blockDim.x + threadIdx.x;
     va[idx] = 'x'; // Threads in the same warp access fields a in a contiguous way from the memory.
     vb[idx] = idx;
@@ -39,8 +41,39 @@ __global__ void mykernel (char *va, int *vb, float *vc) {
 
 int main(int argc, char const *argv[])
 {
-    node_t *v;
-    node_t_v e;
+    /**
+     * Array of Structures (Not optimal for GPU)
+     */
+    node_t *h_v = (note_t*) malloc (N * sizeof(note_t));
+
+    note_t *d_v;
+
+    cudaMalloc((void**)&d_v, N * sizeof(note_t));
+
+    cudaMemcpy(d_v, h_v, N * sizeof(note_t), cudaMemcpyHostToDevice);
+
+    free(h_v);
+    cudaFree(d_v);
+
+    /**
+     * Structure of Arrays (Optimal for GPU - memory coaleshing)
+     */
+    char *h_va = (char*) malloc (N * sizeof(char));
+    int *h_vb = (int*) malloc (N * sizeof(int));
+    float *h_vc = (float*) malloc (N * sizeof(float));
+
+    note_t_v d_e;
+
+    cudaMalloc((void**)&d_e.va, N * sizeof(char));
+    cudaMalloc((void**)&d_e.vb, N * sizeof(int));
+    cudaMalloc((void**)&d_e.vc, N * sizeof(float));
+
+    cudaMemcpy(d_e.va, h_va, N * sizeof(char), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_e.vb, h_vb, N * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(d_e.vc, h_vc, N * sizeof(float), cudaMemcpyHostToDevice);
+
+    free(h_va); free(h_vb); free(h_vc);
+    cudaFree(d_e);
 
     return 0;
 }
